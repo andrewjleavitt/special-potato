89ae7e2715c372ae025da7170fcbeae91922bf39
Andy Leavitt <andyleavitt@gmail.com>
2016-06-04 09:34:34 -0600

    initial commit - creating coach profiles

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.5'
+gem 'mysql2', '>= 0.3.13', '< 0.5'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'therubyracer', platforms: :ruby
+gem 'devise'
+
+# Use jquery as the JavaScript library
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'bcrypt', '~> 3.1.7'
+
+# Use Unicorn as the app server
+gem 'unicorn'
+
+# Use Capistrano for deployment
+gem 'capistrano-rails', group: :development
+
+group :development do
+  gem 'spring'
+end
+
---
+++ app/controllers/coaches_controller.rb
@@ -0,0 +1,43 @@
+class CoachesController < ActionController::Base
+  def index
+    @coaches = Coach.all
+  end
+
+  def new
+    @coach = Coach.new
+  end
+
+  def show
+    @coach = Coach.find(params[:id])
+  end
+
+  def edit
+    @coach = Coach.find(params[:id])
+  end
+
+  def create
+    @coach = Coach.new(coach_params)
+
+    if @coach.save
+      redirect_to @coach
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @coach = Coach.find(params[:id])
+
+    if @coach.update(coach_params)
+      redirect_to @coach
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def coach_params
+    params.require(:coach).permit(:first_name, :last_name)
+  end
+end
