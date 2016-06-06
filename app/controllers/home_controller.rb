84c406ab48e7e0fc342515b3e62d2097c5790007
Andy Leavitt <andyleavitt@gmail.com>
2016-06-05 22:09:33 -0600

    Basic pusher functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ActionController::Base
   def index
+  end
 
+  def hello_world
+    Pusher.trigger('test_channel', 'my_event', {
+        message: 'hello world'
+    })
   end
 end
---
+++ config/initializers/pusher.rb
@@ -0,0 +1,7 @@
+require 'pusher'
+
+Pusher.app_id = '157983'
+Pusher.key = '165dd8173aaf2fd96771'
+Pusher.secret = 'e145fcb7e82c053d792d'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
