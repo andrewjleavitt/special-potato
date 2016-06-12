b7a4f23291802d3728e22211180e2f1c291df01c
Andy Leavitt <andyleavitt@gmail.com>
2016-06-12 11:52:10 -0600

    Add students

---
+++ app/models/student.rb
@@ -0,0 +1,7 @@
+class Student < ActiveRecord::Base
+  has_secure_password
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  resources :students
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
